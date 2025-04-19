19198653c7047d63c58166c8068334081574965b
jafty <japhet.situmonana@gmail.com>
2025-04-19 00:21:51 +0000

    Stripe success cancel

--- zanmi_project/events_app/urls.py
+++ zanmi_project/events_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import event_detail, join_event, manage_participation, notifications_view, featured_event, landing, stripe_webhook
+from .views import event_detail, join_event, manage_participation, notifications_view, featured_event, landing, stripe_webhook, stripe_success
 from django.shortcuts import redirect
 
 
@@ -16,5 +16,7 @@ urlpatterns = [
     path("events/<int:event_id>/manage/", manage_participation, name="manage_participation"),
     path('featured_event/', featured_event, name='featured_event'),
     path("stripe_webhook/", stripe_webhook, name="stripe_webhook"),
+    path("stripe_success/", stripe_success, name="stripe_success"),
+    path("stripe_cancel/", stripe_cancel, name="stripe_cancel"),
 
 ]
