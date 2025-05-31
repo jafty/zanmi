fd17573e46b5d5b000e5db22485d138d00e3c5c6
jafty <japhet.situmonana@gmail.com>
2025-05-31 12:46:02 +0000

    Auto compress and home image fix

--- zanmi_project/events_app/models.py
+++ zanmi_project/events_app/models.py
@@ -1,6 +1,9 @@
 # events_app/models.py
 from django.db import models
 from django.conf import settings
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
 
 class EventDB(models.Model):
     title = models.CharField(max_length=255, blank=True, null=True, unique=True)
@@ -15,6 +18,25 @@ class EventDB(models.Model):
 
     def __str__(self):
         return f"Event by {self.organizer.username} on {self.start_datetime}"
+
+    def save(self, *args, **kwargs):
+        if self.image and not self.image.name.endswith(".webp"):
+            self.image = self.compress_and_convert_image(self.image)
+        super().save(*args, **kwargs)
+
+    def compress_and_convert_image(self, uploaded_image, max_width=1600, quality=80):
+        img = Image.open(uploaded_image)
+        img = img.convert("RGB")  # pour éviter les problèmes avec les PNGs
+
+        if img.width > max_width:
+            ratio = max_width / float(img.width)
+            height = int((float(img.height) * float(ratio)))
+            img = img.resize((max_width, height), Image.ANTIALIAS)
+
+        buffer = BytesIO()
+        img.save(buffer, format="WEBP", quality=quality, optimize=True)
+        new_filename = uploaded_image.name.rsplit(".", 1)[0] + ".webp"
+        return ContentFile(buffer.getvalue(), name=new_filename)
 
 
 class ParticipationDB(models.Model):
